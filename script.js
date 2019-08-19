f42f2d9842c20abb10464c6293cc6d935a056268
Nikida Heller <53826007+nheller1@users.noreply.github.com>
2019-08-19 11:25:42 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>What To Wear</h2>
+<p>Based on weather conditions and event type, this is what you should wear.</p>
+
+<p> Event Type </p>
+<button type="button" onclick="document.write(something comfy)">Casual</button>
+
+<button type="button" onclick="document.write(a polo)">Semi-Formal</button>
+
+<button type="button" onclick="document.write(a suit)">Formal</button>
+
+<p> Temperature </p>
+
+<button type="button" onclick="document.write(a coat)">Less than 54 degrees Fahrenheit</button>
+
+<button type="button" onclick="document.write(a jacket)">54 - 70 degrees Fahrenheit</button>
+
+<button type="button" onclick="document.write(no jacket)">more than 70 degrees Fahrenheit</button>
+
+</body>
+</html> 
